c2de176fa558b4141d36c608e050d91124212ef7
hokumus <hokumus@lab2-ogr>
2019-11-03 12:11:11 +0200

    base entity

--- src/com/hokumus/course/dao/utils/ABaseDbOperations.java
+++ src/com/hokumus/course/dao/utils/ABaseDbOperations.java
@@ -1,10 +1,12 @@
 package com.hokumus.course.dao.utils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.hokumus.course.utils.MyHbUtil;
 
@@ -70,7 +72,7 @@ public abstract class ABaseDbOperations<T> implements IBaseDbOperation<T> {
 			return null;
 		}
 	}
-	
+
 	@Override
 	public boolean delete(T temp) {
 		try {
@@ -82,6 +84,149 @@ public abstract class ABaseDbOperations<T> implements IBaseDbOperation<T> {
 			e.printStackTrace();
 			closeSessionForRollback();
 			return false;
+		}
+	}
+
+	@Override
+	public T find(T temp, int id) {
+		try {
+			openSession();
+			Criteria cr = ss.createCriteria(temp.getClass());
+			cr.add(Restrictions.eq("id", id));
+			T t = (T) cr.list().get(0);
+			closeSession();
+			return t;
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
+		}
+	}
+
+	@Override
+	public List<T> seacrh(String kolonadi, String veri, T temp) {
+		try {
+			openSession();
+			Criteria cr = ss.createCriteria(temp.getClass());
+			cr.add(Restrictions.eq(kolonadi, veri));
+			List<T> liste = cr.list();
+			closeSession();
+			return liste;
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
+		}
+	}
+
+	@Override
+	public List<T> searchRecord(T temp) {
+		try {
+			List<T> listem = null;
+
+			Class cl = temp.getClass();
+			Field[] fl = cl.getDeclaredFields();
+
+			openSession();
+			Criteria cr = ss.createCriteria(temp.getClass());
+			for (int i = 0; i < fl.length; i++) {
+				fl[i].setAccessible(true);
+				if (fl[i].get(temp) != null && !fl[i].get(temp).toString().equals("0")) {
+
+					cr.add(Restrictions.ilike(fl[i].getName(), "%" + fl[i].get(temp) + "%"));
+
+				}
+			}
+			listem = cr.list();
+			closeSession();
+			return listem;
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
+		}
+	}
+	
+	@Override
+	public T findUser(T temp) {
+		try {
+			Class cl = temp.getClass();
+			if (cl.getName().equals("com.hokumus.course.management.model.kullanici.Users")) {
+				Field[] fl = cl.getDeclaredFields();
+				openSession();
+				Criteria cr = ss.createCriteria(temp.getClass());
+				for (int i = 0; i < fl.length; i++) {
+					fl[i].setAccessible(true);
+					if (fl[i].getName().equals("userName") || fl[i].getName().equals("password")) {
+						if (fl[i].get(temp) != null && !fl[i].get(temp).toString().equals("0")) {
+							cr.add(Restrictions.eq(fl[i].getName(), fl[i].get(temp)));
+
+						}
+					}
+
+				}
+				return (T) cr.uniqueResult();
+
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
+		}
+
+	}
+	
+	@Override
+	public T findUserOthers(T temp) {
+		try {
+			Class cl = temp.getClass();
+			Field[] fl = cl.getDeclaredFields();
+			openSession();
+			Criteria cr = ss.createCriteria(temp.getClass());
+			for (int i = 0; i < fl.length; i++) {
+				fl[i].setAccessible(true);
+				if (fl[i].getName().equals("ad") || fl[i].getName().equals("soyad")) {
+					if (fl[i].get(temp) != null && !fl[i].get(temp).toString().equals("0")) {
+						cr.add(Restrictions.eq(fl[i].getName(), fl[i].get(temp)));
+
+					}
+				}
+
+			}
+			return (T) cr.uniqueResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
+		}
+
+	}
+
+	@Override
+	public List<T> findSavedId(T temp) {
+		try {
+			List<T> listem = null;
+
+			Class cl = temp.getClass();
+			Field[] fl = cl.getDeclaredFields();
+
+			openSession();
+			Criteria cr = ss.createCriteria(temp.getClass());
+			for (int i = 0; i < fl.length; i++) {
+				fl[i].setAccessible(true);
+				if (fl[i].get(temp) != null && !fl[i].get(temp).toString().equals("0")) {
+					cr.add(Restrictions.eq(fl[i].getName(), fl[i].get(temp)));
+
+				}
+			}
+			listem = cr.list();
+			return listem;
+		} catch (Exception e) {
+			e.printStackTrace();
+			closeSessionForRollback();
+			return null;
 		}
 	}
 
--- src/com/hokumus/course/dao/utils/IBaseDbOperation.java
+++ src/com/hokumus/course/dao/utils/IBaseDbOperation.java
@@ -7,6 +7,12 @@ public interface IBaseDbOperation<T> {
 	public boolean update(T temp);
 	public boolean delete(T temp);
 	public List<T> getAll(T temp);
+	public List<T> findSavedId(T temp);
+	public T findUserOthers(T temp);
+	public T findUser(T temp);
+	public List<T> searchRecord(T temp);
+	public List<T> seacrh(String kolonadi, String veri, T temp);
+	public T find(T temp, int id);
 	
 
 }
